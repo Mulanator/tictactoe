c814be1791f8e6aa4e07c9f8b6924168ae85ea9b
Jörg Reisslein <joerg@reisslein.de>
2020-09-12 15:34:23 +0200

    120920

--- main.py
+++ main.py
@@ -2,9 +2,21 @@ import sys
 import pygame
 import tictactoe
 
-DIMENSION = [1344,756]
-ROT = (0,0,255)
-BLAU = (255,0,0)
+#Hilfsfunktion, um Text formatiert auszugeben und nicht alle zeilen bei jedem Text nochmal schreiben zu müssen
+def renderText(parameterText, parameterSchriftGröße, parameterSchriftArt, parameterSchriftFarbe):
+    schriftart = pygame.font.SysFont(parameterSchriftArt, parameterSchriftGröße)
+    gerenderterText = schriftart.render(parameterText, 1, parameterSchriftFarbe)
+    return gerenderterText
+
+
+#Konstanten (d.h. Werte die sich nie verändern)
+BREITE = 1344
+HÖHE = 756
+DIMENSION = [BREITE,HÖHE]  #x/y Koordinatensystem, (0,0) ist links oben!
+BLAU = (0,0,255)
+ROT = (255,0,0)
+SCHWARZ = (0,0,0)
+WEISS = (255,255,255)
 
 #pygame und Fenster initialisieren
 pygame.init()
@@ -13,20 +25,14 @@ fenster = pygame.display.set_mode(DIMENSION) #Auflösung gibt das Hintergrundbil
 zeitgeber = pygame.time.Clock()
 pygame.display.set_caption("TicTacToe mit pygame") #Fenstertitel setzen
 
-#Schriftart vorbereiten
-schriftfarbe = ROT #RGB Werte für Rot, Grün, Blau
-schriftart = pygame.font.SysFont("Arial", 80)
-überschrift = "Mein erstes kleines Spiel in Python"
-gerenderteÜbeschrift = schriftart.render(überschrift, 1, schriftfarbe)
-schriftart = pygame.font.SysFont("Arial", 50)
-text = "Drücke die Leertaste"
-textgröße = schriftart.size(text)
-schriftfarbe = BLAU
-gerenderterText = schriftart.render(text, 1, schriftfarbe)
+#Texte fürs Hauptmenü vorbereiten
+gerenderteÜbeschrift = renderText("Jonas erstes kleines Spiel in Python", 80, "Arial", ROT)
+gerenderterText = renderText("Drücke die Leertaste", 50, "Arial", BLAU)
 
-hauptmenü = True #Hilfsvariable für die nachfolgende while-Schleife
+#Hilfsvariable für die nachfolgende while-Schleife
+hauptmenü = True
 
-#Hauptschleife, wird solange ausgeführt bis man im Fenster oben rechts auf das X klickt oder ESCAPE drückt
+#Hauptmenüschleife, wird solange ausgeführt bis man im Fenster oben rechts auf das X klickt oder ESCAPE drückt
 while hauptmenü:
     fenster.blit(hintergrund, (0,0))
     fenster.blit(gerenderteÜbeschrift, (180,300))
@@ -38,6 +44,63 @@ while hauptmenü:
             sys.exit()
         if ereignis.type == pygame.KEYUP and ereignis.key == pygame.K_SPACE:
             hauptmenü = False
-            spieler1 = tictactoe.Spieler
-            spieler2 = tictactoe.Spieler
-            pass #TODO hier neuen Screen initalisieren und tictactoe brett anzeigen
+            imSpiel = True
+
+#jetzt wird das Hauptmenü mit dem Spielbrett ausgetauscht
+#Notwendige Variablen und Angaben für das eigentliche Spiel
+spieler1 = tictactoe.Spieler(1)
+spieler2 = tictactoe.Spieler(2)
+spielbrett = tictactoe.Spielbrett()
+aktiverSpieler = spieler1  #das muss sich bei jedem Zug auf den anderen Spieler ändern
+spieler1.grafik = pygame.image.load("grafiken/X.png")
+spieler2.grafik = pygame.image.load("grafiken/O.png")
+gewonnen = False
+unentschieden = False
+linienfarbe = SCHWARZ
+fenster.fill(WEISS)
+#vertikalen Linien vom Spielfeld zeichnen
+pygame.draw.line(fenster, linienfarbe, (round(BREITE / 3), 0), (round(BREITE / 3), HÖHE), 7)
+pygame.draw.line(fenster, linienfarbe, (round(BREITE / 3 * 2), 0), (round(BREITE / 3 * 2), HÖHE), 7)
+#horizontalen Linien vom Spielfeld zeichnen
+pygame.draw.line(fenster, linienfarbe, (0, round(HÖHE / 3)), (BREITE, round(HÖHE / 3)), 7)
+pygame.draw.line(fenster, linienfarbe, (0, round(HÖHE / 3 * 2)), (BREITE, round(HÖHE / 3 * 2)), 7)
+
+#Spielschleife, wird solange ausgeführt bis man im Fenster oben rechts auf das X klickt oder ESCAPE drückt
+while imSpiel:
+    pygame.draw.rect(fenster, WEISS, (1,1, 200,40)) #weiße Fläche zur Anzeige des aktiven Spielers schaffen
+    gerenderterText = renderText("Aktiver Spieler: " + format(aktiverSpieler.nummer), 20, "Arial", SCHWARZ)
+    fenster.blit(gerenderterText, (1, 1))
+    pygame.display.update()
+    zeitgeber.tick(60) #60 Bilder pro Sekunde (fps)
+    for ereignis in pygame.event.get():  # Ereignisse abfragen
+        if ereignis.type == pygame.QUIT or (ereignis.type == pygame.KEYUP and ereignis.key == pygame.K_ESCAPE):
+            sys.exit()
+        elif ereignis.type is pygame.MOUSEBUTTONDOWN:
+            feld = tictactoe.spieler_mausklick(pygame, BREITE, HÖHE)  #erhält als antwort (spalte, zeile) jeweils 1-3
+            print(feld)  # debug ausgabe auf konsole
+            #prüfen ob Zug erlaubt ist
+            if spielbrett.pruefeSpielfeld(feld) == True and (not gewonnen or not unentschieden):
+                print(spielbrett.holeLetztePosition()) #debug
+                # Grafik in das feld zeichnen
+                spielbrett.setzeSpielfeld(spielbrett.holeLetztePosition(),
+                                          aktiverSpieler.nummer, aktiverSpieler.grafik, fenster, BREITE, HÖHE)
+                #Hier wird nun geprüft ob das SPiel gewonnen wurde
+                if spielbrett.prüfeSpielGewonnen(aktiverSpieler.nummer):
+                    gewonnen = True
+                    gerenderterText = renderText("Gewonnen hat Spieler " + format(aktiverSpieler.nummer), 100, "Arial", ROT)
+                    fenster.blit(gerenderterText, (300, 300))
+                #ist überhaupt noch ein Zug möglich (oder unentschieden?)
+                if spielbrett.istUnentschieden():
+                    unentschieden = True
+                    gerenderterText = renderText("Unentschieden", 120, "Arial", ROT)
+                    fenster.blit(gerenderterText, (300, 300))
+                #aktiven Spieler wechseln
+                if aktiverSpieler.nummer == 1:
+                    aktiverSpieler = spieler2
+                else:
+                    aktiverSpieler = spieler1
+            else:
+                print("Spiel vorbei oder ungültiger Zug erkannt auf Feld", spielbrett.holeLetztePosition())
+
+
+
--- tictactoe.py
+++ tictactoe.py
@@ -1,3 +1,125 @@
+def spieler_mausklick(pygame, breite, hoehe):
+    # Mauskoordinaten abfragen
+    x, y = pygame.mouse.get_pos()
+
+    # Spalte bestimmen (1-3)
+    if (x < breite / 3):
+        spalte = 1
+    elif (x < breite / 3 * 2):
+        spalte = 2
+
+    elif (x < breite):
+        spalte = 3
+    else:
+        spalte = None
+
+    # Zeile bestimmen (1-3)
+    if (y < hoehe / 3):
+        zeile = 1
+
+    elif (y < hoehe / 3 * 2):
+        zeile = 2
+
+    elif (y < hoehe):
+        zeile = 3
+
+    else:
+        zeile = None
+
+    return spalte, zeile
+
 class Spieler:
-    öffentlich = "irgendein öffentliches Attribut der Klasse"
-    __geschützt = "irgendein privates Attribut der Klasse"
+    nummer = None
+    __geschützt = "irgendein privates Attribut der Klasse" #nur als Beispiel
+    grafik = None
+
+    #Konstruktor
+    def __init__(self, nummer):
+        self.nummer = nummer
+
+class Spielbrett:
+
+    __spielfeld = [0,0,0,0,0,0,0,0,0] #3x3 Felder also 9 Elemente, 0 ist links oben, 9 = rechts unten
+    __letztePosition = None
+    def __init__(self):
+        pass
+
+    def pruefeSpielfeld(self, koordinaten):
+        spalte = koordinaten[0]
+        zeile = koordinaten[1]
+        if zeile == 1:
+            position = spalte
+        if zeile == 2:
+            position = spalte + 3
+        if zeile == 3:
+            position = spalte + 6
+        self.__letztePosition = position
+        return (self.__spielfeld[position - 1] == 0)  #Achtung! Index beginnt bei 0 daher 1 abziehen
+
+    def holeLetztePosition(self):
+        return self.__letztePosition
+
+    def setzeSpielfeld(self, position, spielernummer, grafik, fenster, breite, höhe):
+        self.__spielfeld[position - 1] = spielernummer #Achtung! Index beginnt bei 0 daher 1 abziehen
+        zbreite = (round(breite / 3) / 3)
+        zhoehe = round(höhe / 3) / 3
+        if position == 1:
+            x = zbreite
+            y = zhoehe
+        if position == 2:
+            x = zbreite + round(breite / 3)
+            y = zhoehe
+        if position == 3:
+            x = zbreite +  3 * zbreite + round(breite / 3)
+            y = zhoehe
+        if position == 4:
+            x = zbreite
+            y = zhoehe + round(höhe / 3)
+        if position == 5:
+            x = zbreite + round(breite / 3)
+            y = zhoehe + round(höhe / 3)
+        if position == 6:
+            x = zbreite +  3 * zbreite + round(breite / 3)
+            y = zhoehe + round(höhe / 3)
+        if position == 7:
+            x = zbreite
+            y = zhoehe +  2 * round(höhe / 3)
+        if position == 8:
+            x = zbreite + round(breite / 3)
+            y = zhoehe +  2 * round(höhe / 3)
+        if position == 9:
+            x = zbreite +  3 * zbreite + round(breite / 3)
+            y = zhoehe +  2 * round(höhe / 3)
+
+        fenster.blit(grafik, (x, y))
+
+    def prüfeSpielGewonnen(self, aktiverspieler):
+        #index beginnt bei 0, spielfeld für menschen bei 1, also ist der höchste index 8 und nicht 9
+        #erste reihe , felder 1-3, komplett?
+        if (self.__spielfeld[0] == aktiverspieler) and (self.__spielfeld[1] == aktiverspieler) and (self.__spielfeld[2] == aktiverspieler):
+            return True
+        # zweite reihe , felder 4-6, komplett?
+        if (self.__spielfeld[3] == aktiverspieler) and (self.__spielfeld[4] == aktiverspieler) and (self.__spielfeld[5] == aktiverspieler):
+            return True
+        # dritte reihe , felder 7-9, komplett?
+        if (self.__spielfeld[6] == aktiverspieler) and (self.__spielfeld[7] == aktiverspieler) and (self.__spielfeld[8] == aktiverspieler):
+            return True
+        # erste spalte , felder 1,4,7 komplett?
+        if (self.__spielfeld[0] == aktiverspieler) and (self.__spielfeld[3] == aktiverspieler) and (self.__spielfeld[6] == aktiverspieler):
+            return True
+        # zweite spalte , felder 2,5,8 komplett?
+        if (self.__spielfeld[1] == aktiverspieler) and (self.__spielfeld[4] == aktiverspieler) and (self.__spielfeld[7] == aktiverspieler):
+            return True
+        # dritte spalte , felder 3,6,9 komplett?
+        if (self.__spielfeld[2] == aktiverspieler) and (self.__spielfeld[5] == aktiverspieler) and (self.__spielfeld[8] == aktiverspieler):
+            return True
+        #Diagonale über felder 1,5,9 komplett?
+        if (self.__spielfeld[0] == aktiverspieler) and (self.__spielfeld[4] == aktiverspieler) and (self.__spielfeld[8] == aktiverspieler):
+            return True
+        #Diagonale über felder 3,5,7 komplett?
+        if (self.__spielfeld[2] == aktiverspieler) and (self.__spielfeld[4] == aktiverspieler) and (self.__spielfeld[6] == aktiverspieler):
+            return True
+        return False;
+
+    def istUnentschieden(self):
+        return (0 not in self.__spielfeld)
